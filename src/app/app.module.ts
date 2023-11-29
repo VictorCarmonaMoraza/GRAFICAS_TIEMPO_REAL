f55b0b7a6fa890e1bb384a960d6dfb5cabfb74e2
Victor <VictorCarmona9035@hotmail.com>
2023-11-29 22:37:57 +0100

    Inicio Proyecto- Grafica en tiempo real

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+const config: SocketIoConfig = { url: environment.wsUrl, options: {} };
+
 import { AppComponent } from './app.component';
+import { NgChartsModule } from 'ng2-charts';
+
+
+import { environment } from 'src/environments/environment';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config),
+    NgChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
